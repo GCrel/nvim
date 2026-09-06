79661c1beb3786540dbe6c6d0902feba80d11a20
GCrel <omarcoronel3825@gmail.com>
2026-09-06 15:02:44 -0300

    se cambio nvim-tree por neo-tree (Me gusta mas como muestra para modificar o agregar archivos)

---
+++ lua/plugins/editor/neo-tree.lua
@@ -0,0 +1,63 @@
+return {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        "nvim-tree/nvim-web-devicons",
+        "MunifTanjim/nui.nvim",
+    },
+
+    keys = {
+        { "<C-n>",     "<cmd>Neotree toggle<CR>", desc = "Toggle Neo-tree" },
+        { "<leader>e", "<cmd>Neotree focus<CR>",  desc = "Focus Neo-tree" },
+    },
+
+    opts = {
+        close_if_last_window = true,
+        sources = { "filesystem" },
+        use_libuv_file_watcher = true,
+
+        window = {
+            position = "right",
+            width = 30,
+            auto_expand_width = true,
+        },
+
+        filesystem = {
+            group_empty_dirs = true,
+            hijack_netrw_behavior = "open_default",
+            bind_to_cwd = true,
+            follow_current_file = {
+                enabled = true,
+            },
+            filtered_items = {
+                hide_dotfiles = false,
+                hide_gitignored = false,
+            },
+        },
+
+        default_component_configs = {
+            name = {
+                trailing_slash = false,
+                use_git_status_colors = true,
+            },
+            git_status = {
+                symbols = {
+                    unstaged = "✗",
+                    staged = "✓",
+                    modified = "",
+                    renamed = "➜",
+                    untracked = "★",
+                    deleted = "",
+                    ignored = "◌",
+                },
+            },
+            icon = {
+                folder_closed = "󰉋",
+                folder_open = "",
+                folder_empty = "",
+                default = "󰈚",
+            },
+        },
+    },
+}
