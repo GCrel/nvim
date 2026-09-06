300af840430d18fd4d66c3ccf0f7441dad7271f7
GCrel <omarcoronel3825@gmail.com>
2026-09-06 15:01:42 -0300

    se agrego el español para ltex_plus

--- lua/plugins/lsp/servers.lua
+++ lua/plugins/lsp/servers.lua
@@ -12,10 +12,10 @@ return {
         local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
         require("mason-lspconfig").setup({
-            ensure_installed = { "lua_ls", "jdtls" },
+            ensure_installed = { "lua_ls", "jdtls", "ltex_plus" },
 
             automatic_enable = {
-                exclude = { "jdtls" },
+                exclude = { "jdtls", "ltex_plus" },
             },
 
             handlers = {
@@ -41,6 +41,19 @@ return {
                             },
                         },
                     })
+                end,
+
+                ["ltex_plus"] = function()
+                    require("lspconfig").ltex.setup({
+                        capabilities = capabilities,
+                        filetypes = { "markdown", "text", "gitcommit" },
+                        settings = {
+                            ltex = {
+                                language = "es-AR",
+                            },
+                        },
+                    }
+                    )
                 end,
             },
         })
