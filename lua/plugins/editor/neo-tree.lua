ce8fb24c27c5acd381d58bc6cc5748e5c90fab6f
GCrel <omarcoronel3825@gmail.com>
2026-09-17 14:19:53 -0300

    se Movio la funcionalidad de file-Operations al arcvhivo de neo-tree

--- lua/plugins/editor/neo-tree.lua
+++ lua/plugins/editor/neo-tree.lua
@@ -5,6 +5,10 @@ return {
         "nvim-lua/plenary.nvim",
         "nvim-tree/nvim-web-devicons",
         "MunifTanjim/nui.nvim",
+        {
+            "antosha417/nvim-lsp-file-operations",
+            config = true,
+        },
     },
 
     keys = {
